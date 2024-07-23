3d5308343dc7dfe64a1787081343dcb26141d594
furkangocuk <furkangocuk42@gmail.com>
2024-07-23 15:09:19 +0300

    stylized widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+import 'dart:ui';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -57,52 +60,92 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Column(
         children: [
-          TextField(
-            autocorrect: false,
-            keyboardType: TextInputType.emailAddress,
-            controller: _email,
-            decoration: const InputDecoration(hintText: "Enter your e-mail"),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Material(
+              elevation: 7.0,
+              shadowColor: Color.fromARGB(75, 255, 0, 0),
+              child: TextField(
+                autocorrect: false,
+                keyboardType: TextInputType.emailAddress,
+                controller: _email,
+                decoration: const InputDecoration(
+                    hintText: "Enter your e-mail",
+                    contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                    border: OutlineInputBorder()),
+              ),
+            ),
           ),
-          TextField(
-            obscureText: true,
-            obscuringCharacter: "*",
-            enableSuggestions: false,
-            autocorrect: false,
-            controller: _password,
-            decoration: const InputDecoration(hintText: "Enter your password"),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Material(
+              elevation: 7.0,
+              shadowColor: Color.fromARGB(75, 255, 0, 0),
+              child: TextField(
+                  obscureText: true,
+                  obscuringCharacter: "*",
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  controller: _password,
+                  decoration: const InputDecoration(
+                      hintText: "Enter your password",
+                      contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                      border: OutlineInputBorder())),
+            ),
           ),
-          TextButton(
-            onPressed: () async {
-              final email = _email.text;
-              final password = _password.text;
-              try {
-                final userCredential = await FirebaseAuth.instance
-                    .createUserWithEmailAndPassword(
-                        email: email, password: password);
-                print(userCredential);
-              } catch (e) {
-                print("Registration Error: $e");
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: const Text("Error"),
-                      content: Text(e.toString()),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
+          const SizedBox(height: 30),
+          ClipRRect(
+              borderRadius: BorderRadius.circular(5),
+              child: Stack(
+                children: <Widget>[
+                  Positioned.fill(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                          gradient: LinearGradient(colors: [
+                        Colors.red,
+                        Color.fromARGB(255, 234, 91, 81)
+                      ])),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      final email = _email.text;
+                      final password = _password.text;
+                      try {
+                        final userCredential = await FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                                email: email, password: password);
+                        print(userCredential);
+                      } catch (e) {
+                        print("Registration Error: $e");
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: const Text("Error"),
+                              content: Text(e.toString()),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text("OK"),
+                                ),
+                              ],
+                            );
                           },
-                          child: const Text("OK"),
-                        ),
-                      ],
-                    );
-                  },
-                );
-              }
-            },
-            child: const Text("Register"),
-          ),
+                        );
+                      }
+                    },
+                    child: const Text("Register"),
+                    style: TextButton.styleFrom(
+                        padding: const EdgeInsets.only(
+                            top: 15, bottom: 15, left: 30, right: 30),
+                        foregroundColor: Colors.white,
+                        textStyle: const TextStyle(fontSize: 15)),
+                  )
+                ],
+              ))
         ],
       ),
     );

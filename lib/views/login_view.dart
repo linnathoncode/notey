97c517a3cd7dba288a7b27a4187bd0c84052e05d
furkangocuk <furkangocuk42@gmail.com>
2024-07-24 15:42:29 +0300

    added login view

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,37 +1,14 @@
-import 'dart:ui';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:notey/views/login_view.dart';
-import 'firebase_options.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Notey',
-      theme: ThemeData(
-        colorScheme:
-            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 163, 5, 5)),
-        useMaterial3: true,
-      ),
-      home: const LoginView(),
-    ),
-  );
-}
-
-class RegisterView extends StatefulWidget {
-  const RegisterView({super.key});
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
 
   @override
-  State<RegisterView> createState() => _RegisterViewState();
+  State<LoginView> createState() => _LoginViewState();
 }
 
-class _RegisterViewState extends State<RegisterView> {
+class _LoginViewState extends State<LoginView> {
   late final TextEditingController _email;
   late final TextEditingController _password;
 
@@ -53,8 +30,8 @@ class _RegisterViewState extends State<RegisterView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Register"),
-        backgroundColor: Colors.indigo,
+        title: const Text("Login"),
+        backgroundColor: Color.fromARGB(255, 210, 68, 55),
         foregroundColor: Colors.white,
       ),
       body: Column(
@@ -112,17 +89,17 @@ class _RegisterViewState extends State<RegisterView> {
                       final password = _password.text;
                       try {
                         final userCredential = await FirebaseAuth.instance
-                            .createUserWithEmailAndPassword(
+                            .signInWithEmailAndPassword(
                                 email: email, password: password);
                         print(userCredential);
-                      } on FirebaseException catch (e) {
+                      } on FirebaseAuthException catch (e) {
                         print("Registration Error: $e");
                         showDialog(
                           context: context,
                           builder: (context) {
                             return AlertDialog(
                               title: const Text("Error"),
-                              content: Text(e.toString()),
+                              content: Text(e.code),
                               actions: [
                                 TextButton(
                                   onPressed: () {
@@ -136,7 +113,7 @@ class _RegisterViewState extends State<RegisterView> {
                         );
                       }
                     },
-                    child: const Text("Register"),
+                    child: const Text("Login"),
                     style: TextButton.styleFrom(
                         padding: const EdgeInsets.only(
                             top: 15, bottom: 15, left: 30, right: 30),

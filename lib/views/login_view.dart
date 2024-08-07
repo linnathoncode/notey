f2b0856e7d6f1e1e6a2f67bf1676afa8369c0404
furkangocuk <furkangocuk42@gmail.com>
2024-08-07 16:19:48 +0300

    fixed login view error message

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -12,6 +12,7 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  final _formKey = GlobalKey<FormState>();
   late final TextEditingController _email;
   late final TextEditingController _password;
 
@@ -33,194 +34,205 @@ class _LoginViewState extends State<LoginView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Center(child: Text('Login'))),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(5),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Color.fromARGB(75, 255, 0, 0),
-                      blurRadius: 7.0,
-                      offset: Offset(0, 3),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Color.fromARGB(75, 255, 0, 0),
+                          blurRadius: 7.0,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                child: TextFormField(
-                  autocorrect: false,
-                  keyboardType: TextInputType.emailAddress,
-                  controller: _email,
-                  decoration: const InputDecoration(
-                    hintText: "Enter your e-mail",
-                    contentPadding: EdgeInsets.symmetric(horizontal: 20),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(5)),
-                      borderSide: BorderSide.none,
-                    ),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(5),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Color.fromARGB(75, 255, 0, 0),
-                      blurRadius: 7.0,
-                      offset: Offset(0, 3),
-                    ),
-                  ],
-                ),
-                child: TextFormField(
-                  autocorrect: false,
-                  keyboardType: TextInputType.visiblePassword,
-                  controller: _password,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    hintText: "Enter your password",
-                    contentPadding: EdgeInsets.symmetric(horizontal: 20),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(5)),
-                      borderSide: BorderSide.none,
-                    ),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-            ),
-            const SizedBox(height: 8),
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(5),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Color.fromARGB(75, 255, 0, 0),
-                      blurRadius: 7.0,
-                      offset: Offset(0, 3),
-                    ),
-                  ],
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(5),
-                  child: Stack(
-                    children: <Widget>[
-                      Positioned.fill(
-                        child: Container(
-                          decoration: const BoxDecoration(
-                            gradient: LinearGradient(
-                              colors: [
-                                Colors.red,
-                                Color.fromARGB(255, 234, 91, 81)
-                              ],
-                            ),
-                          ),
+                    child: TextFormField(
+                      autocorrect: false,
+                      keyboardType: TextInputType.emailAddress,
+                      controller: _email,
+                      decoration: const InputDecoration(
+                        hintText: "Enter your e-mail",
+                        contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(5)),
+                          borderSide: BorderSide.none,
                         ),
                       ),
-                      TextButton(
-                        onPressed: () async {
-                          final email = _email.text;
-                          final password = _password.text;
-                          try {
-                            final userCredential = await FirebaseAuth.instance
-                                .signInWithEmailAndPassword(
-                                    email: email, password: password);
-                            if (context.mounted) {
-                              devtools.log(userCredential.toString());
-                              ScaffoldMessenger.of(context)
-                                  .showSnackBar(SnackBar(
-                                content: Center(
-                                  child: Text(
-                                      "Logged in as ${userCredential.user?.email}",
-                                      style:
-                                          const TextStyle(color: Colors.white)),
-                                ),
-                                duration: const Duration(seconds: 2),
-                                backgroundColor: Colors.green,
-                              ));
-                              await Navigator.of(context)
-                                  .pushNamedAndRemoveUntil(
-                                notesRoute,
-                                (route) => false,
-                              );
-                            }
-                          } on FirebaseAuthException catch (e) {
-                            String errorMessage;
-                            switch (e.code) {
-                              case 'invalid-email':
-                                errorMessage =
-                                    'The email address is not valid.';
-                                break;
-                              case 'user-disabled':
-                                errorMessage = 'The user has been disabled.';
-                                break;
-                              case 'user-not-found':
-                                errorMessage = 'No user found with this email.';
-                                break;
-                              case 'wrong-password':
-                                errorMessage = 'Incorrect password.';
-                                break;
-                              case 'invalid-credential':
-                                errorMessage = 'Invalid credential.';
-                              default:
-                                errorMessage = 'An unknown error occurred.';
-                            }
-                            if (context.mounted) {
-                              ScaffoldMessenger.of(context)
-                                  .showSnackBar(SnackBar(
-                                content: Center(
-                                    child: Text(errorMessage,
-                                        style: const TextStyle(
-                                            color: Colors.white))),
-                                duration: const Duration(seconds: 2),
-                                backgroundColor: Colors.red,
-                              ));
-                            }
-                          }
-                        },
-                        style: TextButton.styleFrom(
-                            padding: const EdgeInsets.only(
-                                top: 15, bottom: 15, left: 30, right: 30),
-                            foregroundColor: Colors.white,
-                            textStyle: const TextStyle(fontSize: 15)),
-                        child: const Text("Login"),
-                      ),
-                    ],
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your email';
+                        }
+                        return null;
+                      },
+                    ),
                   ),
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Color.fromARGB(75, 255, 0, 0),
+                          blurRadius: 7.0,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: TextFormField(
+                      autocorrect: false,
+                      keyboardType: TextInputType.visiblePassword,
+                      controller: _password,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        hintText: "Enter your password",
+                        contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(5)),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your password';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Color.fromARGB(75, 255, 0, 0),
+                          blurRadius: 7.0,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(5),
+                      child: Stack(
+                        children: <Widget>[
+                          Positioned.fill(
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                gradient: LinearGradient(
+                                  colors: [
+                                    Colors.red,
+                                    Color.fromARGB(255, 234, 91, 81)
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () async {
+                              if (_formKey.currentState?.validate() ?? false) {
+                                final email = _email.text;
+                                final password = _password.text;
+                                try {
+                                  final userCredential = await FirebaseAuth
+                                      .instance
+                                      .signInWithEmailAndPassword(
+                                          email: email, password: password);
+                                  if (context.mounted) {
+                                    devtools.log(userCredential.toString());
+                                    ScaffoldMessenger.of(context)
+                                        .showSnackBar(SnackBar(
+                                      content: Center(
+                                        child: Text(
+                                            "Logged in as ${userCredential.user?.email}",
+                                            style: const TextStyle(
+                                                color: Colors.white)),
+                                      ),
+                                      duration: const Duration(seconds: 2),
+                                      backgroundColor: Colors.green,
+                                    ));
+                                    await Navigator.of(context)
+                                        .pushNamedAndRemoveUntil(
+                                      notesRoute,
+                                      (route) => false,
+                                    );
+                                  }
+                                } on FirebaseAuthException catch (e) {
+                                  String errorMessage;
+                                  switch (e.code) {
+                                    case 'invalid-email':
+                                      errorMessage =
+                                          'The email address is not valid.';
+                                      break;
+                                    case 'user-disabled':
+                                      errorMessage =
+                                          'The user has been disabled.';
+                                      break;
+                                    case 'user-not-found':
+                                      errorMessage =
+                                          'No user found with this email.';
+                                      break;
+                                    case 'wrong-password':
+                                      errorMessage = 'Incorrect password.';
+                                      break;
+                                    case 'invalid-credential':
+                                      errorMessage = 'Invalid credential.';
+                                    default:
+                                      errorMessage =
+                                          'An unknown error occurred.';
+                                  }
+                                  if (context.mounted) {
+                                    ScaffoldMessenger.of(context)
+                                        .showSnackBar(SnackBar(
+                                      content: Center(
+                                          child: Text(errorMessage,
+                                              style: const TextStyle(
+                                                  color: Colors.white))),
+                                      duration: const Duration(seconds: 2),
+                                      backgroundColor: Colors.red,
+                                    ));
+                                  }
+                                }
+                              }
+                            },
+                            style: TextButton.styleFrom(
+                                padding: const EdgeInsets.only(
+                                    top: 15, bottom: 15, left: 30, right: 30),
+                                foregroundColor: Colors.white,
+                                textStyle: const TextStyle(fontSize: 15)),
+                            child: const Text("Login"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamedAndRemoveUntil(
+                        registerRoute,
+                        (route) => false,
+                      );
+                    },
+                    child: const Text('Not registered yet?')),
+              ],
             ),
-            TextButton(
-                onPressed: () {
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    registerRoute,
-                    (route) => false,
-                  );
-                },
-                child: const Text('Not registered yet?')),
-          ],
+          ),
         ),
       ),
     );
